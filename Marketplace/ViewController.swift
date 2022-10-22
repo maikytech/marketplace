3d896fafc07aef49c809f753a89cc656c571df0d
Maiqui Cedeño <maiqui.cedeno@gc.vasslatam.com>
2022-10-21 20:37:48 -0500

    Start project

---
+++ Marketplace/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Marketplace
+//
+//  Created by Maiqui Cedeño on 21/10/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
